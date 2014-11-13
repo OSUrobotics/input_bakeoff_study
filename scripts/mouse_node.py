e565333d3e7d2135f130fe490a914655c19e31d7
Dan Lazewatsky <dan@lazewatsky.com>
2014-11-13 12:37:54 -0600

    don't require face detection to be running

--- scripts/mouse_node.py
+++ scripts/mouse_node.py
@@ -29,6 +29,7 @@ def pose_cb(pose):
     if initialized:
         px = int(angle_to_pixel(yaw, left, right, 0, mouse.screen_size()[0]))
         py = int(angle_to_pixel(pitch, top, bottom, 0, mouse.screen_size()[1]))
+        print px, py
         mouse.move(px, py)
 
 
@@ -42,8 +43,9 @@ def angle_to_pixel(ang, ang_min, ang_max, px_min, px_max):
 if __name__ == '__main__':
     rospy.init_node('pose_to_mouse')
     tf_listener = tf.TransformListener()
-    fixed_frame = rospy.wait_for_message('/face_detector/faces_cloud', PointCloud).header.frame_id
-    rospy.Subscriber('/head_pose', PoseStamped, pose_cb)
+    head_pose_frame = rospy.wait_for_message('/head_pose', PoseStamped).header.frame_id
+    tf_listener.waitForTransform(head_pose_frame, 'face_detection', rospy.Time(0), rospy.Duration(10))
+    rospy.Subscriber('/head_pose', PoseStamped, pose_cb, queue_size=1)
 
     print 'Move your head to the top left corner of the screen and click'
     rospy.wait_for_message('click', Empty)
